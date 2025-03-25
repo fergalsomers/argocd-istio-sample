9752a5d900b4f17c55f7f6040ed34a5b024014f4
Fergal Somers <fsomers@gmail.com>
2025-03-25 18:50:10 +0000

    Update the readme

--- install-argocd-platform.sh
+++ install-argocd-platform.sh
@@ -8,7 +8,7 @@ set -e
 kubectl apply -k boot-application
 
 echo "ArgoCD will now load the platform application.."
-echo "This may take 10-15 minutes.."
+echo "This may take up to 15 minutes.."
 echo " - you can check the status of the application by running (in another terminal):"
 echo " > kubectl get applications -n argocd -w"
 echo ".. meanwhile waiting for the platform application to be healthy..." 
@@ -33,3 +33,11 @@ echo " - Grafana provide metrics observability for the platform"
 echo " - you will need to login with the username 'admin' and the default password is also 'admin'"
 echo " - you will be prompted to change the password on first login (of skip this step)"
 echo " - All of the Kubernetes Mixin dashboards are available in the 'http://127.0.0.1:8081/dashboards' folder"
+
+echo "You can also access Jaeger to view traces originating from the Ingress Gateway (i.e. your browser traffic). "
+echo " - to setup local port-frorwarding to Jaeger, run:"
+echo " > kubectl port-forward -n istio-system svc/tracing 8080:80 &"
+echo 
+echo " - you can then access Jaeger at http://localhost:8080/" to view traces in Jaeger UI
+
+echo "Loading the platform via ArgoCD is complete. It took:"
